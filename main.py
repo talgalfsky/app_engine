b501430c7cc8b1a4994ee00f50000b4815fc0fa0
talgalfsky <45616255+talgalfsky@users.noreply.github.com>
2021-12-18 17:09:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from flask_restful import Resource, Api
+from googleapiclient import discovery
+from google.cloud import bigquery
+from oauth2client.client import GoogleCredentials
+client = bigquery.Client()
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route("/")
 def homepage():
@@ -13,6 +19,23 @@ def docs():
 @app.route("/about")
 def about():
     return render_template("index.html", title="about page")
+
+class PrintNames(Resource):
+    def get(self):
+        client = bigquery.Client()
+        query = """
+        SELECT * 
+        FROM `sbx-nameswipe-1.tal_dev.baby_features` 
+        LIMIT 5
+        """
+        query_res = client.query(query)
+
+        results = {} #empty dic
+        for row in query_res:
+            results[row.name] = row.name
+        return({'res': results},200)
+
+api.add_resource(PrintNames, '/printnames')  # and '/printnames' is our entry point for printnames
 
 if __name__ == "__main__":
     app.run(debug=True)
